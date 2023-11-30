59bf1dccf1981ac38aeb29a7505ca8cd27ba2b35
kasturada1 <kazuki1.katsurada@gmail.com>
2023-11-30 16:55:20 +0900

    Modify select_tweet_info.sql

--- postgres/docker-entrypoint-initdb.d/select_tweet_info.sql
+++ postgres/docker-entrypoint-initdb.d/select_tweet_info.sql
@@ -2,17 +2,18 @@ WITH retweet_count AS (
     SELECT COUNT(*) AS count
     FROM retweets
     WHERE tweet_id = 0
-)
-
-WITH like_count AS (
+), 
+like_count AS (
     SELECT COUNT(*) AS count
     FROM likes
     WHERE tweet_id = 0
 )
 
-SELECT text, icon_path, retweet_count.count, like_count.count created_time, updated_time
+SELECT text, icon_path, retweet_count.count, like_count.count, created_time, updated_time
     FROM tweets
     JOIN retweet_count
+    ON tweets.id = 0
+    JOIN like_count
     ON tweets.id = 0
     JOIN users
     ON tweets.user_id = users.id
